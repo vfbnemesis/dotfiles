ffe092acd19cae00474cc9570f0f68b970278a60
vfbnemesis <vfbnemsis@yandex.ru>
2016-10-06 23:30:41 +0700

    add my minor mode (for keybinding)

---
+++ .emacs.d/plugins/my-mode.el
@@ -0,0 +1,33 @@
+﻿;; Main use is to have my key bindings have the highest priority
+;; https://github.com/kaushalmodi/.emacs.d/blob/master/elisp/modi-mode.el
+
+(defvar my-mode-map (make-sparse-keymap)
+  "Keymap for `my-mode'.")
+
+;;;###autoload
+(define-minor-mode my-mode
+  "A minor mode so that my key settings override annoying major modes."
+  ;; If init-value is not set to t, this mode does not get enabled in
+  ;; `fundamental-mode' buffers even after doing \"(global-my-mode 1)\".
+  ;; More info: http://emacs.stackexchange.com/q/16693/115
+  :init-value t
+  :lighter " my-mode"
+  :keymap my-mode-map)
+
+;;;###autoload
+(define-globalized-minor-mode global-my-mode my-mode my-mode)
+
+;; https://github.com/jwiegley/use-package/blob/master/bind-key.el
+;; The keymaps in `emulation-mode-map-alists' take precedence over
+;; `minor-mode-map-alist'
+(add-to-list 'emulation-mode-map-alists `((my-mode . ,my-mode-map)))
+
+;; Turn off the minor mode in the minibuffer
+(defun turn-off-my-mode ()
+  "Turn off my-mode."
+  (my-mode -1))
+(add-hook 'minibuffer-setup-hook #'turn-off-my-mode)
+
+(provide 'my-mode)
+
+;; Minor mode tutorial: http://nullprogram.com/blog/2013/02/06/
--- .emacs.d/settings/settings.el
+++ .emacs.d/settings/settings.el
@@ -155,7 +155,7 @@
 (setq theme-current my-color-themes)
 (my-theme-set-default)
 
-(global-set-key [f7] 'my-theme-cycle)
+(global-set-key (kbd "<f7>") 'my-theme-cycle)
 
 ;; --- hide/show C block ------------------------------------------------------
 (defvar hs-special-modes-alist
@@ -308,6 +308,10 @@
 ;; --- load plugins -----------------------------------------------------------
 
 (add-to-list 'load-path "~/.emacs.d/plugins")
+
+;; my minor mode
+;; в основном для клавиаттурных сочетаний
+(require 'my-mode)
 
 ;; номера строк (слева)
 ;; http://code.google.com/p/dea/source/browse/trunk/my-lisps/linum%2B.el
@@ -499,12 +503,56 @@ down                _i_: ace maximize
 
 (use-package multiple-cursors
   :ensure t
-  :config
+  :bind (:map my-mode-map
+              ("C-S-c C-S-c" . mc/edit-lines)
+              ("C->" . mc/mark-next-like-this)
+              ("C-<" . mc/mark-previous-like-this)
+              ("C-c C-<" . mc/mark-all-like-this)
+              ("C-S-<mouse-1>" . mc/add-cursor-on-click))
+  :bind (:map region-bindings-mode-map
+              ("a" . mc/mark-all-like-this)
+              ("p" . mc/mark-previous-like-this)
+              ("n" . mc/mark-next-like-this)
+              ("P" . mc/unmark-previous-like-this)
+              ("N" . mc/unmark-next-like-this)
+              ("[" . mc/cycle-backward)
+              ("]" . mc/cycle-forward)
+              ("m" . mc/mark-more-like-this-extended)
+              ("h" . mc-hide-unmatched-lines-mode)
+              ("\\" . mc/vertical-align-with-space)
+              ("#" . mc/insert-numbers) ; use num prefix to set the starting number
+              ("^" . mc/edit-beginnings-of-lines)
+              ("$" . mc/edit-ends-of-lines))
+  :init
   (progn
-    (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-    (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-    (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-    (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)    
+    ;; (setq mc/list-file (locate-user-emacs-file "mc-lists"))
+
+    ;; Disable the annoying sluggish matching paren blinks for all cursors
+    ;; when you happen to type a ")" or "}" at all cursor locations.
+    (defvar modi/mc-blink-matching-paren--store nil
+      "Internal variable used to restore the value of `blink-matching-paren'
+after `multiple-cursors-mode' is quit.")
+
+    ;; The `multiple-cursors-mode-enabled-hook' and
+    ;; `multiple-cursors-mode-disabled-hook' are run in the
+    ;; `multiple-cursors-mode' minor mode definition, but they are not declared
+    ;; (not `defvar'd). So do that first before using `add-hook'.
+    (defvar multiple-cursors-mode-enabled-hook nil
+      "Hook that is run after `multiple-cursors-mode' is enabled.")
+    (defvar multiple-cursors-mode-disabled-hook nil
+      "Hook that is run after `multiple-cursors-mode' is disabled.")
+
+    (defun modi/mc-when-enabled ()
+      "Function to be added to `multiple-cursors-mode-enabled-hook'."
+      (setq modi/mc-blink-matching-paren--store blink-matching-paren)
+      (setq blink-matching-paren nil))
+
+    (defun modi/mc-when-disabled ()
+      "Function to be added to `multiple-cursors-mode-disabled-hook'."
+      (setq blink-matching-paren modi/mc-blink-matching-paren--store))
+
+    (add-hook 'multiple-cursors-mode-enabled-hook #'modi/mc-when-enabled)
+    (add-hook 'multiple-cursors-mode-disabled-hook #'modi/mc-when-disabled)
     ))
 
 
